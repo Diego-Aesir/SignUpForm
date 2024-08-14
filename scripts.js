e29f9891706a98c93c29039bc545193b3344188d
Diego-Aesir <diego.s.galvao91@gmail.com>
2024-08-13 20:19:28 -0400

    Project finished.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,31 @@
 const buttonSend = document.querySelector("#sendForm");
 const passwordInput = document.querySelector("#passwordLabel");
+const confirmPasswordInput = document.querySelector("#confirmPasswordLabel");
+const passwordText = document.querySelector(".passwordText");
+const confirmPasswordText = document.querySelector(".confirmPassworText");
 
-console.log(passwordInput.value);
+passwordText.className = "disabled";
 
-buttonSend.addEventListener("click", () => {
+buttonSend.addEventListener("click", validatePassword);
+
+function validatePassword() {
     if(passwordInput.value === "") {
-        window.alert("Por favor, escreva uma senha");
+        passwordInput.className = "invalid";
+        passwordText.innerHTML = "Escreva uma senha para este campo acima!";
+        passwordText.className = "enabled";
+    } else if(passwordInput.value != "" && confirmPasswordInput.value == "") {
+        passwordText.innerHTML = "Por favor, confirme sua senha!";
+        confirmPasswordText.className = "enabled"
+        confirmPasswordInput.className = "invalid";
+        passwordInput.className = "invalid";
+    } else if (passwordInput.value != confirmPasswordInput.value) {
+        passwordText.innerHTML = "Por favor, escreva a mesma senha nos campos!";
+        confirmPasswordInput.className = "invalid";
+        passwordInput.className = "invalid";
+    } else {
+        passwordText.className = "disabled";
+        confirmPasswordInput.className = "valid";
+        passwordInput.className = "valid";
+        window.alert("Obrigado, este e o seu primeiro passo para o corpo perfeito");
     }
-});
-
+}
